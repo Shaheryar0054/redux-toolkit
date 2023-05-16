d8368b4733adbe5eb11d87930203b2f8febec4c4
shaheryar abid <sharyar0310@gmail.com>
2023-05-16 22:07:51 +0500

    Added increase decrease and remove all function

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,34 @@
-import{ createSlice} from "@reduxjs/toolkit";
-
+import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 const initialState = {
-    cartItems: [],
-    amount:0,
-    total:0,
-    isLoading:true,
+  cartItems: cartItems,
+  amount: 4,
+  total: 0,
+  isLoading: true,
 };
+
 const cartSlice = createSlice({
-    name:"cart",
-    initialState,
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem:(state,action) =>{
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item)=>item.id !==itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+  },
 });
 
-// console.log(cartSlice);
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
